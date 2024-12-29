a4768be844a0b60d2389675d957732518d79d218
LeVietDuongg <22520300@gmail.com>
2024-12-30 00:09:41 +0700

    hoàn chỉnh

--- app/src/main/java/com/example/detectapplication2/MapFragment.java
+++ app/src/main/java/com/example/detectapplication2/MapFragment.java
@@ -200,14 +200,7 @@ public class MapFragment extends Fragment {
         });
         //Khi người dùng chọn một kết quả trong danh sách hiển thị
         searchResultsList.setOnItemClickListener((parent, view1, position, id) -> {
-            getActivity().runOnUiThread(() -> {
-                // Clear the user selected marker
-                if (userSelectedMarker != null) {
-                    mapView.getMapScene().removeMapMarker(userSelectedMarker);
-                    userSelectedMarker = null;
-                }
-                userSelectedCoordinates = null; // Clear the user selected coordinates
-            });
+          setRouteButton.setVisibility(View.GONE); // Show the button when a location is selected
             GeoCoordinates selectedCoordinates = searchResultsCoordinates.get(position);
             mapView.getGestures().setTapListener(touchPoint -> {
                 Point2D point2D = new Point2D(touchPoint.x, touchPoint.y);
